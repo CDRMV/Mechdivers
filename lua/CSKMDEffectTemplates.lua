2b12d15ecbe6ecff4ab50ec748197efb884591d0
CDRMV <cdrmv2017@gmail.com>
2025-07-12 09:08:14 +0200

    Mechdivers: Reorganize Github File Structure

--- lua/CSKMDEffectTemplates.lua
+++
@@ -1,6 +0,0 @@
-ModBpPath = '/mods/Mechdivers/effects/emitters/'
-EmtBpPath = '/effects/emitters/'
-EmitterTempEmtBpPath = '/effects/emitters/temp/'
-TableCat = import('/lua/utilities.lua').TableCat
-
-
