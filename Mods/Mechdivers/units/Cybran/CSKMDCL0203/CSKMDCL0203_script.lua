a0305edb84ba1308532fcd262cf478259908452c
CDRMV <cdrmv2017@gmail.com>
2025-09-30 10:08:30 +0200

    Mechdivers: Rocket Devastator Animation Fix + Balance

--- Mods/Mechdivers/units/Cybran/CSKMDCL0203/CSKMDCL0203_script.lua
+++ Mods/Mechdivers/units/Cybran/CSKMDCL0203/CSKMDCL0203_script.lua
@@ -16,7 +16,42 @@ local CIFGrenadeWeapon = import('/lua/cybranweapons.lua').CIFGrenadeWeapon
 CSKMDCL0203 = Class(CWalkingLandUnit) {
     Weapons = {
         MainGun = Class(CDFLaserFusionWeapon) {},
-		MissileRack = Class(CIFGrenadeWeapon) {},
+		MissileRack = Class(CIFGrenadeWeapon) {
+		
+		-------------------------------------------- 
+		-- Missilelauncher Unpack and Pack Sequence
+		--------------------------------------------
+		-- To Fix an Walk Animation Issue caused by the Weapon Unpack Animation is not getting deleted by the Game itself.
+		-- We need to do that manually with the Code below.
+		------------------------------------------------------------------------------------------------------------------
+		
+		PlayFxWeaponUnpackSequence = function(self)
+			if self.WeaponPackState == 'Unpacked' then
+		
+			else
+			local bp = self.Blueprint
+			self.UnpackAnimator = CreateAnimator(self.unit)
+			self.UnpackAnimator:PlayAnim('/mods/Mechdivers/units/Cybran/CSKMDCL0203/CSKMDCL0203_AUnpack2.sca'):SetRate(2)
+			self.WeaponPackState = 'Unpacking'
+			WaitFor(self.UnpackAnimator)
+			self.WeaponPackState = 'Unpacked'
+			end
+		end,
+
+		PlayFxWeaponPackSequence = function(self)
+			local bp = self.Blueprint
+			local unpackAnimator = self.UnpackAnimator
+			if unpackAnimator and self.WeaponPackState == 'Unpacked' then
+				unpackAnimator:SetRate(-2)
+				self.WeaponPackState = 'Packing'
+				WaitFor(unpackAnimator)
+				unpackAnimator:Destroy()
+				self.unit:SetImmobile(false)
+			end	
+			self.WeaponPackState = 'Packed'
+		end,
+		
+		},
     },
 	
 	OnCreate = function(self)
