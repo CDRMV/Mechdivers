d45eb160f68d100c11aed259549389533b546b14
CDRMV <cdrmv2017@gmail.com>
2026-02-14 18:12:36 +0100

    Mechdivers: UEF Bastion Tankhunter (WIP)

---
+++ Mods/Mechdivers/units/UEF/CSKMDTL0308/CSKMDTL0308_Script.lua
@@ -0,0 +1,22 @@
+#****************************************************************************
+#**
+#**  File     :  /cdimage/units/UEL0201/UEL0201_script.lua
+#**  Author(s):  John Comes, David Tomandl, Jessica St. Croix
+#**
+#**  Summary  :  UEF Medium Tank Script
+#**
+#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+#****************************************************************************
+
+local TLandUnit = import('/lua/defaultunits.lua').MobileUnit
+local TDFGaussCannonWeapon = import('/lua/terranweapons.lua').TDFGaussCannonWeapon
+local TDFMachineGunWeapon = import('/lua/terranweapons.lua').TDFMachineGunWeapon
+
+CSKMDTL0308 = Class(TLandUnit) {
+    Weapons = {
+        MainGun = Class(TDFGaussCannonWeapon) {},
+		MachineGun = Class(TDFMachineGunWeapon) {}
+    },
+}
+
+TypeClass = CSKMDTL0308
