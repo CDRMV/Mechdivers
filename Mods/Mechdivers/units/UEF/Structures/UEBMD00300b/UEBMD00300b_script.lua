916181c37767fd94f3853f8020044ca7f44f7ef2
CDRMV <cdrmv2017@gmail.com>
2026-07-09 21:22:21 +0200

    Mechdivers: FAF Patch

--- Mods/Mechdivers/units/UEF/Structures/UEBMD00300b/UEBMD00300b_script.lua
+++ Mods/Mechdivers/units/UEF/Structures/UEBMD00300b/UEBMD00300b_script.lua
@@ -8,7 +8,7 @@
 #**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
 #****************************************************************************
 
-local TStructureUnit = import('/lua/defaultunits.lua').StructureUnit
+local TStructureUnit = import('/lua/defaultunits.lua').StructureUnit2
 local TSAMLauncher = import('/lua/terranweapons.lua').TSAMLauncher
 
 UEBMD00300b = Class(TStructureUnit) {
