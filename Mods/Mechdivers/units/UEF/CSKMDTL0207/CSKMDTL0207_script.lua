49e12bc2c675001d195485ab78702a5ac150604a
CDRMV <cdrmv2017@gmail.com>
2026-07-25 18:08:26 +0200

    Mechdivers: UEF Flamethrower Truck (WIP)

---
+++ Mods/Mechdivers/units/UEF/CSKMDTL0207/CSKMDTL0207_script.lua
@@ -0,0 +1,243 @@
+#****************************************************************************
+#**
+#**  File     :  /cdimage/units/UEL0111/UEL0111_script.lua
+#**  Author(s):  John Comes, David Tomandl, Jessica St. Croix
+#**
+#**  Summary  :  UEF Mobile Missile Launcher Script
+#**
+#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+#****************************************************************************
+
+local TLandUnit = import('/lua/defaultunits.lua').MobileUnit
+local TDFMachineGunWeapon = import('/lua/terranweapons.lua').TDFMachineGunWeapon
+local EffectTemplate = import('/lua/EffectTemplates.lua')
+
+CSKMDTL0207 = Class(TLandUnit) {
+
+    Weapons = {
+        Flamethrower = Class(TDFMachineGunWeapon) {
+        },
+    },
+
+	OnStopBeingBuilt = function(self,builder,layer)
+		TLandUnit.OnStopBeingBuilt(self,builder,layer)
+		self.build = true
+		if self:GetAIBrain().BrainType == 'Human' then
+		SetIgnoreArmyUnitCap(self:GetArmy(), true)
+		local position = self:GetPosition()
+		local Bot = CreateUnitHPR('UEL0106', self:GetArmy(), position[1], position[2], position[3], 0, 0, 0)
+		Bot:AttachBoneTo(-2, self, 'Attachpoint')
+		Bot:SetDoNotTarget(true)
+		Bot:SetWeaponEnabledByLabel('ArmCannonTurret', false)
+		Bot:SetUnSelectable(true)
+		Bot:HideBone(0, true)
+		Bot:SetCollisionShape('Box', 0, 0, 0, 0, 0, 0)
+		Bot:RemoveCommandCap('RULEUCC_Attack')
+		Bot:RemoveCommandCap('RULEUCC_RetaliateToggle')
+		Bot:RemoveCommandCap('RULEUCC_Stop')
+		SetIgnoreArmyUnitCap(self:GetArmy(), false)
+		end
+		BotMesh = '/mods/Mechdivers/Decorations/T1Bot_mesh'
+		self.Bot = import('/lua/sim/Entity.lua').Entity()
+        self.Bot:AttachBoneTo( -1, self, 'Attachpoint' )
+        self.Bot:SetMesh(BotMesh)
+        self.Bot:SetDrawScale(0.0625)
+        self.Bot:SetVizToAllies('Intel')
+        self.Bot:SetVizToNeutrals('Intel')
+        self.Bot:SetVizToEnemies('Intel')
+		self:AddToggleCap('RULEUTC_ProductionToggle')
+		self:RemoveCommandCap('RULEUCC_Transport')
+		if not self.AnimationManipulator then
+            self.AnimationManipulator = CreateAnimator(self)
+            self.Trash:Add(self.AnimationManipulator)
+        end
+		self.AnimationManipulator:PlayAnim('/Mods/Mechdivers/units/UEF/CSKMDTL0205/CSKMDTL0205_Door.sca', false):SetRate(0)
+		self.load = true
+    end,
+	
+		OnMotionHorzEventChange = function(self, new, old)
+            TLandUnit.OnMotionHorzEventChange(self, new, old)
+            if ( new == 'Stopped' ) then
+			self:AddToggleCap('RULEUTC_WeaponToggle')
+            elseif ( old == 'Stopped' ) then
+			if self:GetScriptBit('RULEUTC_WeaponToggle') == true then
+			self:SetScriptBit('RULEUTC_WeaponToggle', false)
+			self:RemoveToggleCap('RULEUTC_WeaponToggle')
+			self:RemoveToggleCap('RULEUTC_SpecialToggle')
+			else
+			self:RemoveToggleCap('RULEUTC_WeaponToggle')
+			self:RemoveToggleCap('RULEUTC_SpecialToggle')
+			end
+            end
+        end,
+	
+	DeathThread = function( self, overkillRatio , instigator)  
+		self.Bot:Destroy()
+        self:DestroyAllDamageEffects()
+		local army = self:GetArmy()
+
+		if self.PlayDestructionEffects then
+            self:CreateDestructionEffects(overkillRatio)
+        end
+
+        if self.ShowUnitDestructionDebris and overkillRatio then
+            self:CreateUnitDestructionDebris(true, true, overkillRatio > 2)
+        end
+		
+		self:CreateWreckage(overkillRatio or self.overkillRatio)
+		local RandomNumber = math.random(1, 2)
+		if RandomNumber == 2 then
+		SetIgnoreArmyUnitCap(self:GetArmy(), true)
+		local position = self:GetPosition()
+		local Nanites = CreateUnitHPR('UEL0106', self:GetArmy(), position[1], position[2], position[3], 0, 0, 0)
+		SetIgnoreArmyUnitCap(self:GetArmy(), false)
+		end
+        self:PlayUnitSound('Destroyed')
+        self:Destroy()
+    end,
+	
+	OnScriptBitSet = function(self, bit)
+        TLandUnit.OnScriptBitSet(self, bit)
+		 if bit == 1 then 
+		self.AnimationManipulator:SetRate(1)
+		SetIgnoreArmyUnitCap(self:GetArmy(), true)
+		local position = self:GetPosition('BeaconPos')
+		self.Beacon = CreateUnitHPR('UEB5102', self:GetArmy(), position[1], position[2], position[3], 0, 0, 0)
+		SetIgnoreArmyUnitCap(self:GetArmy(), false)
+		self:AddToggleCap('RULEUTC_SpecialToggle')
+		self:AddToggleCap('RULEUTC_IntelToggle')
+		elseif bit == 7 then 
+		if self.build == true then
+		self:SetScriptBit('RULEUTC_SpecialToggle', false)
+		self.build = false
+		LOG('Test')
+		else
+		self.load = true
+		local position = self.Beacon:GetPosition()
+			local units = self.Beacon:GetAIBrain():GetUnitsAroundPoint(categories.MOBILE + categories.LAND + categories.TECH1, position, 10, 'Ally')
+			local number = 0
+        for _,unit in units do
+			if unit:IsUnitState('WaitForFerry') and unit:GetBlueprint().General.UnitName == '<LOC uel0106_name>Mech Marine' then
+			if number < 1 then
+			unit:AttachBoneTo(-2, self, 'Attachpoint')
+			unit:SetDoNotTarget(true)
+			unit:SetWeaponEnabledByLabel('ArmCannonTurret', false)
+			unit:SetUnSelectable(true)
+			unit:HideBone(0, true)
+			unit:SetCollisionShape('Box', 0, 0, 0, 0, 0, 0)
+			unit:RemoveCommandCap('RULEUCC_Attack')
+			unit:RemoveCommandCap('RULEUCC_RetaliateToggle')
+			unit:RemoveCommandCap('RULEUCC_Stop')
+			IssueClearCommands({unit})
+			self.Bot:SetVizToAllies('Intel')
+			self.Bot:SetVizToNeutrals('Intel')
+			self.Bot:SetVizToEnemies('Intel')
+			self:AddCommandCap('RULEUCC_Attack')
+			self:AddCommandCap('RULEUCC_RetaliateToggle')
+			self:SetWeaponEnabledByLabel('MainGun', true)
+			number = number + 1
+			else
+			end
+			else
+            end
+		end
+		end		
+		elseif bit == 3 then
+		self.Beacon:HideBone(0, true)		
+		elseif bit == 4 then
+		self:SetSpeedMult(2)
+		end
+    end,
+
+    OnScriptBitClear = function(self, bit)
+        TLandUnit.OnScriptBitClear(self, bit)
+		if bit == 1 then 
+		self:RemoveToggleCap('RULEUTC_IntelToggle')
+		self:RemoveToggleCap('RULEUTC_SpecialToggle')
+		self.Beacon:Destroy()
+		self.AnimationManipulator:SetRate(-1)
+		elseif bit == 7 then
+		self.load = false
+		local units = self:GetCargo()
+		local position = self:GetPosition()
+		self.Bot:SetVizToAllies('Never')
+		self.Bot:SetVizToNeutrals('Never')
+		self.Bot:SetVizToEnemies('Never')
+        for _, unit in units do
+			Warp(unit, {position[1] + math.random(-1,1), GetTerrainHeight(position[1], position[3]), position[3] + math.random(-1,1)}, self:GetOrientation())
+			unit:ShowBone(0, true)
+			unit:SetDoNotTarget(false)
+			unit:SetUnSelectable(false)
+			unit:SetWeaponEnabledByLabel('ArmCannonTurret', true)
+			unit:SetCollisionShape('Box', 0, 0,0, 0.45, 0.55, 0.35)
+			unit:DetachFrom(true)
+			unit:AddCommandCap('RULEUCC_Attack')
+			unit:AddCommandCap('RULEUCC_RetaliateToggle')
+			unit:AddCommandCap('RULEUCC_Stop')
+			self:RemoveCommandCap('RULEUCC_Attack')
+			self:RemoveCommandCap('RULEUCC_RetaliateToggle')
+			self:SetWeaponEnabledByLabel('MainGun', false)
+        end	
+		elseif bit == 3 then
+		self.Beacon:ShowBone(0, true)
+		elseif bit == 4 then
+		self:SetSpeedMult(1)
+		end
+    end,
+	
+	
+	OnKilled = function(self, instigator, type, overkillRatio)
+	if self.Beacon then
+	self.Beacon:Destroy()
+	end	
+	
+	if self.load == false then
+	
+	else
+	if self.Bot then
+	self.Bot:Destroy()
+	end
+	local RandomNumber = math.random(1, 2)
+	if RandomNumber == 2 then
+	SetIgnoreArmyUnitCap(self:GetArmy(), true)
+	local position = self:GetPosition()
+	local orientation = self:GetOrientation()
+	local angle = 2 * math.acos(orientation[2])
+	self.unit = CreateUnitHPR('UEL0106', self:GetArmy(), position[1], position[2], position[3], 0, angle, 0)
+	SetIgnoreArmyUnitCap(self:GetArmy(), false)
+	end
+	end
+	
+	
+    TLandUnit.OnKilled(self, instigator, type, overkillRatio)	
+    end,
+	
+	OnReclaimed = function(self, reclaimer)
+		if self.Beacon then
+		self.Beacon:Destroy()
+		end
+		
+		if self.load == false then
+	
+		else
+		if self.Bot then
+			self.Bot:Destroy()
+			units[1]:ShowBone(0, true)
+			units[1]:SetDoNotTarget(false)
+			units[1]:SetUnSelectable(false)
+			units[1]:SetWeaponEnabledByLabel('ArmCannonTurret', true)
+			units[1]:SetCollisionShape('Box', 0, 0,0, 0.45, 0.55, 0.35)
+			units[1]:DetachFrom(true)
+			units[1]:AddCommandCap('RULEUCC_Attack')
+			units[1]:AddCommandCap('RULEUCC_RetaliateToggle')
+			units[1]:AddCommandCap('RULEUCC_Stop')
+		else
+		
+		end
+		
+		end
+    end,
+	
+}
+
+TypeClass = CSKMDTL0207
